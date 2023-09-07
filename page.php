22fc1945c0e4ed6039d268c072d47261b5ae62ec
Milyra35 <marie.richir35@gmail.com>
2023-09-07 15:55:07 +0200

    Finally resolved the issue about the articles

--- page.php
+++ page.php
@@ -2,7 +2,7 @@
 	$menuItems = getNavigationMenu();
     $data = getHomepageData(); 
 	$events = getEvents();
-	var_dump($events);
+	// var_dump($events);
 ?>
 
 <?php get_header();?>
@@ -10,20 +10,6 @@
 <main>
 	<h2><?php the_title();?></h2>
 
-	<?php
-		foreach($events as $event)
-		{
-			echo '
-				<article class="event">
-					<h3>'.get_the_title($event).'</h3>
-					<p>'.tribe_get_start_date($event, false, 'Y-m-d H:i').'</p>
-					<p>'.$event->post_content.'</p>
-					<p>'.tribe_get_venue($event).'</p>
-					<a href='.$event->guid.'>Page évènement</a>
-				</article>
-			';
-		}
-	?>
 </main>
 
 <?php get_footer();?>
